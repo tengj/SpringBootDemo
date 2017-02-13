c147d5127d6224595b6a1bdd94cf4ebaf976f2b0
tengj <358593266@qq.com>
2017-02-13 16:28:39 +0800

    提交第一个spring boot项目

---
+++ springBootDemo1/src/main/java/com/tengj/SpringBootDemo1Application.java
@@ -0,0 +1,12 @@
+package com.tengj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo1Application.class, args);
+	}
+}
