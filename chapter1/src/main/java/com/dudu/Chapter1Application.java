db667347973ff878fe769dc70d5ae298582ee3b2
tengj <358593266@qq.com>
2017-02-25 22:39:45 +0800

    第一章Spring Boot入门

---
+++ chapter1/src/main/java/com/dudu/Chapter1Application.java
@@ -0,0 +1,19 @@
+package com.dudu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+
+public class Chapter1Application {
+	@RequestMapping("/")
+	public String index(){
+		return "Hello Spring Boot";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+}
