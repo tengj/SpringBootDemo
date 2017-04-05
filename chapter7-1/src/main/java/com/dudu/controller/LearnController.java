ce81832064279b832c1611e9d5211bc69ce8e09b
tengj <358593266@qq.com>
2017-04-05 22:21:58 +0800

    Spring Boot干货系列：（七）默认日志logback配置解析

---
+++ chapter7-1/src/main/java/com/dudu/controller/LearnController.java
@@ -0,0 +1,77 @@
+package com.dudu.controller;
+
+
+import com.dudu.domain.LearnResouce;
+import com.dudu.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by tengj on 2017/3/13.
+ */
+@Controller
+public class LearnController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> login(HttpServletRequest request, HttpServletResponse response){
+        //日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
+        logger.trace("日志输出 trace");
+        logger.debug("日志输出 debug");
+        logger.info("日志输出 info");
+        logger.warn("日志输出 warn");
+        logger.error("日志输出 error");
+        Map<String,Object> map =new HashMap<String,Object>();
+        String userName=request.getParameter("userName");
+        String password=request.getParameter("password");
+        if(!userName.equals("") && password!=""){
+            User user =new User(userName,password);
+            request.getSession().setAttribute("user",user);
+            map.put("result","1");
+        }else{
+            map.put("result","0");
+        }
+        return map;
+    }
+
+    @RequestMapping("/learn")
+    public ModelAndView index(){
+        List<LearnResouce> learnList =new ArrayList<LearnResouce>();
+        LearnResouce bean =new LearnResouce("官方参考文档","Spring Boot Reference Guide","http://docs.spring.io/spring-boot/docs/1.5.1.RELEASE/reference/htmlsingle/#getting-started-first-application");
+        learnList.add(bean);
+        bean =new LearnResouce("官方SpriongBoot例子","官方SpriongBoot例子","https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples");
+        learnList.add(bean);
+        bean =new LearnResouce("龙国学院","Spring Boot 教程系列学习","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResouce("嘟嘟MD独立博客","Spring Boot干货系列 ","http://tengj.top/");
+        learnList.add(bean);
+        bean =new LearnResouce("后端编程嘟","Spring Boot教程和视频 ","http://www.toutiao.com/m1559096720023553/");
+        learnList.add(bean);
+        bean =new LearnResouce("程序猿DD","Spring Boot系列","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResouce("纯洁的微笑","Sping Boot系列文章","http://www.ityouknow.com/spring-boot");
+        learnList.add(bean);
+        bean =new LearnResouce("CSDN——小当博客专栏","Sping Boot学习","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResouce("梁桂钊的博客","Spring Boot 揭秘与实战","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResouce("林祥纤博客系列","从零开始学Spring Boot ","http://412887952-qq-com.iteye.com/category/356333");
+        learnList.add(bean);
+        ModelAndView modelAndView = new ModelAndView("template");
+        modelAndView.addObject("learnList", learnList);
+        return modelAndView;
+    }
+}
