d34aef78868a471e6eb868477761af4474c2ac92
asong <314893771@qq.com>
2020-12-18 13:55:44 +0800

    优化

--- chapter11/src/main/java/com/dudu/domain/User.java
+++ chapter11/src/main/java/com/dudu/domain/User.java
@@ -4,51 +4,72 @@ import javax.persistence.*;
 
 @Table(name = "user")
 public class User {
+    /**
+     * ID
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 用户名
+     */
     private String name;
 
+    /**
+     * 密码
+     */
     private String passwrod;
 
     /**
-     * @return id
+     * 获取ID
+     *
+     * @return id - ID
      */
     public Long getId() {
         return id;
     }
 
     /**
-     * @param id
+     * 设置ID
+     *
+     * @param id ID
      */
     public void setId(Long id) {
         this.id = id;
     }
 
     /**
-     * @return name
+     * 获取用户名
+     *
+     * @return name - 用户名
      */
     public String getName() {
         return name;
     }
 
     /**
-     * @param name
+     * 设置用户名
+     *
+     * @param name 用户名
      */
     public void setName(String name) {
         this.name = name == null ? null : name.trim();
     }
 
     /**
-     * @return passwrod
+     * 获取密码
+     *
+     * @return passwrod - 密码
      */
     public String getPasswrod() {
         return passwrod;
     }
 
     /**
-     * @param passwrod
+     * 设置密码
+     *
+     * @param passwrod 密码
      */
     public void setPasswrod(String passwrod) {
         this.passwrod = passwrod == null ? null : passwrod.trim();
